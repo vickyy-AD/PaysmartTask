a95d01ded0492face0dd0b23fbf44d6929f482d3
vignesh-sjv <vignesh.s@sciflare.com>
2025-12-23 10:56:04 +0530

    Code Submission

--- App.tsx
+++ App.tsx
@@ -1,17 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet } from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigator/AppNavigator';
+import { colors } from './src/utils/colors';
 
 function App() {
  
@@ -19,18 +11,12 @@ function App() {
     <SafeAreaProvider>
      <StatusBar
           translucent
-          backgroundColor="#ff4181"
+          backgroundColor={colors.primary}
           barStyle="light-content"
         />
       <AppNavigator />
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
